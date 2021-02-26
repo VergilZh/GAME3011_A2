7041e11d0250c6898344e0c2501950f08cf40b98
VergilZh <z1205067147@gmail.com>
2021-02-25 20:54:45 -0500

    Initial commit

---
+++ Assets/Script/OpenLock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLock : MonoBehaviour
+{
+    public float Speed = 1;
+    public AudioSource OpenAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, 0, Speed);
+            if (!OpenAudio.isPlaying)
+            {
+                OpenAudio.Play();
+            }
+        }
+        else if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0, -Speed);
+            if (!OpenAudio.isPlaying)
+            {
+                OpenAudio.Play();
+            }
+        }
+        else
+        {
+            OpenAudio.Stop();
+        }
+    }
+}
